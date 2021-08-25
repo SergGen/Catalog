42fee3301ad9f471ad0e161fd24392f8073ac903
Sergey <71509510+SergGen@users.noreply.github.com>
2021-08-25 18:06:13 +0300

    ДЗ 6 Проект разбит на компоненты.

--- script-vue.js
+++ script-vue.js
@@ -1,7 +1,109 @@
 'use strict';
+
+const CartComp = {
+  props: {
+    catalogRender: Array,
+    cartItems: Array,
+  },
+  data(){
+    return {
+      showHideCart: false,
+    }
+  },
+  computed: {
+    totalPrice(){
+      let total = this.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+      return total === 0 ? 'Cart is empty' : total;
+    },
+    showTotalQuantityIcon(){
+      return this.cartItems.length > 0;
+    }
+  },
+  methods: {
+    addItem(idItemAdd){
+      this.$emit('adding', idItemAdd);
+    },
+    removeItem(idItemRm){
+      this.$emit('removing', idItemRm);
+    },
+    subtractItem(item, idItemRm){
+      this.$emit('subtracting', {item, idItemRm});
+    },
+  },
+  template: `
+    <div class="header__cart" @click="showHideCart = !showHideCart">
+    <img src="img/cart.svg" alt="cart" class="header__cart_img">
+    <span class="header__cart_circle" v-show="showTotalQuantityIcon">{{cartItems.length}}</span>
+    </div>
+    <div v-show="showHideCart" class="cart">
+    <button class="cart__btn_close" @click="showHideCart = !showHideCart">X</button>
+    <div class="cart__items">
+      <div class="cart__item" v-for="(item,i) in cartItems" :key="item.id">
+        <div class="cart__img_box"><img :src="'./img/'+item.img" :alt="item.img"></div>
+        <div class="cart__info_box"><p class="cart__item_name">Title: {{item.name}}</p>
+          <p class="cart__item_price">Price: $ {{item.price}}</p>
+          <p class="cart__item_quantity">Quantity: {{item.quantity}}            
+          </p></div>
+        <div class="cart__btns">
+          <button class="add-item" @click="addItem(item.id)">+</button>
+          <button  class="minus-item" @click="subtractItem(item,i)">-</button>
+          <button class="remove-item" @click="removeItem(i)">X</button></div>
+      </div>
+    </div>
+    <p class="total-price">Total price: <span class="total-price_num">{{totalPrice}}</span></p>
+    </div>
+  `
+}
+
+const SearchComp = {
+  data(){
+    return {
+      showHideSearch: false
+    }
+  },
+  methods: {
+      searching(e){
+        this.$emit('searching', e.target.value.trim());
+      }
+  },
+  template: `
+    <button class="header__search _icon-search" @click="showHideSearch = !showHideSearch"></button>
+    <label v-show="showHideSearch">
+      <input type="text" class="search_items" @input="searching"></label>`
+}
+
+const ItemsCatalog = {
+  props: {
+    catalogRender: Array
+  },
+  methods: {
+    addingItem(itemId){
+      this.$emit('adding', itemId);
+    }
+  },
+  template: `
+    <div class="fet-item" v-for="item in this.catalogRender" :key="item.id">
+      <img :src="'img/'+item.img" :alt="item.img" class="fet-item__images">
+      <div class="fet-item__info">
+        <h5 class="fet-item__name">{{item.name}}</h5>
+        <p class="fet-item__description">{{item.description}}</p>
+        <p class="fet-item__price">$ {{item.price}}</p>
+      </div>
+      <div class="fet-item__add-cart_area">
+        <button class="_icon-cart fet-item__add-cart_btn" @click="addingItem(item.id)"><span class="add-cart__text">Add to Cart</span></button>
+      </div>
+    </div>
+  `
+}
+
 Vue.createApp({
+  components: {
+    SearchComp,
+    CartComp,
+    ItemsCatalog
+  },
   data: () => ({
-    API_URL: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
+    apiUrl: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
     catalogRender: [
       {id: 'id1' ,name: "ELLERY X M'O CAPSULE 1", img: 'fi-1.png', price: 51, balance: 2, description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.'},
       {id: 'id8' ,name: "ELLERY X M'O CAPSULE 8", img: 'fi-8.png', price: 52, balance: 3, description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.'},
@@ -17,24 +119,14 @@ Vue.createApp({
       {id: 'id10' ,name: "ELLERY X M'O CAPSULE 10", img: 'fi-10.png', price: 62, balance: 3, description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.'},
     ],
     catalogOrigin: [],
-    showHide: {
-      search: false,
-      cart: false,
-      cartQuantity: false
-    },
     itemToSearch: '',
     cartItems: []
   }),
-  computed: {
-    totalPrice(){
-      let total = this.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
-      if(total === 0) {
-        total = 'Cart is empty';
-      }
-      return total;
-    }
-  },
   methods: {
+    inToSearch(string){
+      this.itemToSearch = string;
+    },
+
     search() {
       if(this.catalogOrigin.length === 0){
         this.catalogOrigin = this.catalogRender.slice();
@@ -47,7 +139,7 @@ Vue.createApp({
       }
     },
     addItem(idItemAdd){
-      fetch(`${this.API_URL}/addToBasket.json`)
+      fetch(`${this.apiUrl}/addToBasket.json`)
         .then(response => response.json())
         .then(answer => {
           if(answer['result']){
@@ -57,7 +149,6 @@ Vue.createApp({
             if(itemInCart.length === 0){
               itemAdded.quantity = 1;
               this.cartItems.push(itemAdded);
-              this.showTotalQuantityIcon();
             } else {
               if(itemInCart[0].quantity < itemInCart[0].balance){
                 itemInCart[0].quantity += 1;
@@ -73,13 +164,12 @@ Vue.createApp({
           console.log(error);
         });
     },
-    removeItem(item, idItemRm){
-      fetch(`${this.API_URL}/deleteFromBasket.json`)
+    removeItem(idItemRm){
+      fetch(`${this.apiUrl}/deleteFromBasket.json`)
         .then(response => response.json())
         .then(answer => {
           if(answer['result']){
             this.cartItems.splice(idItemRm,1);
-            this.showTotalQuantityIcon();
           }else {
             console.log("no server answer by removeItem");
           }
@@ -88,19 +178,16 @@ Vue.createApp({
           console.log(error);
         });
     },
-    subtractItem(item, idItemRm){
-      item.quantity--;
-      if(item.quantity < 1){
-        this.removeItem(item, idItemRm);
+    subtractItem(items){
+      items.item.quantity--;
+      if(items.item.quantity < 1){
+        this.removeItem(items.idItemRm);
       }
     },
-    showTotalQuantityIcon(){
-      this.showHide.cartQuantity = this.cartItems.length > 0;
-    }
   },
   watch:{
     itemToSearch(){
       this.search();
     }
-  }
+  },
 }).mount('body');
